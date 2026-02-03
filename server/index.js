98a11edb04138d68dab4cccbd4113b3715913ba6
Kingsley <this.onenterframe@gmail.com>
2026-02-03 13:15:04 -0700

    Fix static file path - serve from client/ not public/

--- server/index.js
+++ server/index.js
@@ -34,11 +34,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Serve static files from public folder
+// Serve static files from client folder
 import path from 'path';
 import { fileURLToPath } from 'url';
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../client')));
 
 // Redirect root to spectator page
 app.get('/', (req, res) => {
